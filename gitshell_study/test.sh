ec8e6d3224418eb5c1b73926d2086694178608d8
ella <niuyujing1984@163.com>
2021-03-15 13:29:48 +0800

    gitshell_study

--- gitshell_study/test.sh
+++ gitshell_study/test.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
+#thare are some comments as the following:
+# 1. use ${array_name[@]} to get all elements of array_name
+# 2. use ${#array_name[@]} or ${#array_name[*]} to get the length of this array_name
+# 3. use ${#array_name[]n} to get the length of each element for this array_name
+# 4. use # to comment each row
+
 echo "hello world!"
-myUrl="https://www.google.com"
-readonly myUrl
-myUrl="https://www.runoob.com"
